fc55a3f6951499b8e63852fb7124f4ddec4b7ee4
Abrham Tilksew <130549348+Abrish-21@users.noreply.github.com>
2025-02-05 21:27:38 +0300

    Time: 0 ms (100%), Space: 17.8 MB (41.45%) - LeetHub

--- 0812-rotate-string/0812-rotate-string.py
+++ 0812-rotate-string/0812-rotate-string.py
@@ -2,21 +2,5 @@ class Solution:
     def rotateString(self, s: str, goal: str) -> bool:
         if len(s) != len(goal):
             return False
-        else:
-            indx  = 0
-            s_arry = [i for i in s]
-            goal_arry = [i for i in goal]
-            # change both to arrays
-            while indx < len(s):
-                s_arry.append(s_arry[0])
-                del s_arry[0]
-                if(s_arry == goal_arry):
-                    return True
-                indx +=1
-                print(s_arry)
-                print(goal_arry)
-            return False
-
-
-
+        return goal in s + s
         
