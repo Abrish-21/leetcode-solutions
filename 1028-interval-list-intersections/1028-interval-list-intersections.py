f67414f880923293dd7d90ba69817480fff473f0
Abrham Tilksew <130549348+Abrish-21@users.noreply.github.com>
2025-08-14 11:50:44 +0300

    1028-INTERVAL-LIST-INTERSECTIONS Stats: Time: 2 ms (90.78%), Space: 18.5 MB (69.9%) - LeetHub

--- 1028-interval-list-intersections/1028-interval-list-intersections.py
+++ 1028-interval-list-intersections/1028-interval-list-intersections.py
@@ -1,35 +1,18 @@
 class Solution:
     def intervalIntersection(self, firstList: List[List[int]], secondList: List[List[int]]) -> List[List[int]]:
-        f = 0
-        s = 0
+        i = 0
+        j = 0
         output = []
-        while f< len(firstList) and s < len(secondList):
-            if secondList[s][0] <= firstList[f][1] and secondList[s][1] >= firstList[f][1]:
-                output.append([max(firstList[f][0], secondList[s][0]), min(firstList[f][1], secondList[s][1])])
-                if firstList[f][1] < secondList[s][1]:
-                    f += 1
-                elif firstList[f][1] > secondList[s][1]:
-                    s += 1
-                else:
-                    s +=1
-                    f += 1
-            elif firstList[f][0] <= secondList[s][1] and firstList[f][1] >=  secondList[s][1]:
-                output.append([max(firstList[f][0], secondList[s][0]), min(firstList[f][1], secondList[s][1])])
-                if firstList[f][1] < secondList[s][1]:
-                    f += 1
-                elif firstList[f][1] > secondList[s][1]:
-                    s += 1
-                else:
-                    s +=1
-                    f += 1
-            elif firstList[f][1] < secondList[s][0]:
-                f += 1
+        while i< len(firstList) and j < len(secondList):
+            f_start, f_end = firstList[i]
+            s_start, s_end  = secondList[j]
+            if f_start <= s_end and s_start <= f_end:
+                output.append([max(f_start, s_start), min(f_end, s_end)])
+            if f_end <= s_end:
+                i += 1
             else:
-                s += 1
-
-            
+                j += 1
         return output
-
 
 
         
