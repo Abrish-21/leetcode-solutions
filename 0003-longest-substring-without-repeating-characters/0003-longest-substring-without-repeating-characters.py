b248c73a8cec645f8bd2fcc93338e8de6246b61a
Abrham Tilksew <130549348+Abrish-21@users.noreply.github.com>
2025-10-01 20:41:53 +0300

    Time: 10 ms (96.03%), Space: 17.8 MB (82.18%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -6,8 +6,7 @@ class Solution:
         for end in range(len(s)):
             curr_char = s[end]
             if curr_char in store and start <= store[curr_char]:
-                while start <= store[curr_char]:
-                    start += 1
+                start  = store[curr_char] + 1
             longest_s = max(longest_s, end - start + 1)
             store[curr_char] = end
         return longest_s
