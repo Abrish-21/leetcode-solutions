0051972ff14ba58123ceb999125d0c8289620e14
Abrham Tilksew <130549348+Abrish-21@users.noreply.github.com>
2025-10-01 20:20:07 +0300

    Time: 15 ms (80.23%), Space: 17.7 MB (96.77%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.py
@@ -1,25 +1,16 @@
-class Solution(object):
-    def lengthOfLongestSubstring(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        start  = 0
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        store = defaultdict(int)
+        start = 0
         longest_s = 0
-        seen  = defaultdict(int)
         for end in range(len(s)):
-            # //add the the frquency to the map
             curr_char = s[end]
-            if curr_char in seen and seen[curr_char] >= start:
-                start = seen[curr_char] + 1
-
-            seen[curr_char] = end
-
-            # now check compute the longest  length
+            if curr_char in store and start <= store[curr_char]:
+                while start <= store[curr_char]:
+                    start += 1
             longest_s = max(longest_s, end - start + 1)
+            store[curr_char] = end
         return longest_s
             
-
-
-
-        
+                
+    
